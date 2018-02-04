9fcaadd3acaf354ef7002c2067877ad45a3b0734
Davide Arcozzi <daarco@gmail.com>
2018-02-04 18:00:41 +0100

    Initial Commit

---
+++ Bowtie+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Bowtie+CoreDataClass.swift
+//  Bow Ties
+//
+//  Created by Davide Arcozzi on 04/02/18.
+//  Copyright © 2018 Razeware. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Bowtie)
+public class Bowtie: NSManagedObject {
+
+}
